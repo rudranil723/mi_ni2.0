b9b659db43984140afc18e0de5e217bde2cfc837
Rudranil Shil <72538521+rudranil723@users.noreply.github.com>
2022-01-22 13:47:40 +0530

    Update main.py
    update 2

--- mini/main.py
+++ mini/main.py
@@ -1,9 +1,11 @@
 #mini 2.0 date 18.01.2022
 
+from email.mime import audio
 import speech_recognition as sr
 import os
 from gtts import gTTS
 import datetime
+from datetime import datetime
 import math
 import wikipedia
 import warnings
@@ -12,50 +14,60 @@ import random
 
 warnings.filterwarnings('ignore') #this will ignore any unnessary warnings
 
-def text_password(text): #funtion to check password/wake word
-    wake_word =["heye mini","mini","yo mini"] # words take can be used by the user as password/wakewords
-    
+tc=0 #special variable
+
+def password(text): #funtion to check password/wake word
+    wake_word=["hey mini","mini","yo mini","hi mini","okay mini"] # words that can be used by the user as password/wakewords
+    text = text.lower() #converting the input data to lower case 
     for phrase in wake_word:
-        if phrase in wake_word:
+        if phrase == text:
             print("hellow user!!! :)")
             return True #return true if the password entered by the user is correct
-        else:
-            print("you have entered a wrong password, mini won't work BYE.....")
-            return False # the program will not work of the passowrd is wrong
+    print("you have entered a wrong password, mini won't work BYE.....")
+    return False # the program will not work of the passowrd is wrong
 
+if password(text=True):
+    print("wokering")
 
-
-
-
-def voice_password():
-    print("working 2")
-
-
-
-
-
+def recordaudio(): #function to record audio from the user
+    r= sr.Recognizer()
+    with sr.Microphone() as source: #calling in the microphone to record the audio from user
+        print("say the password: ")
+        audio = r.listen(source)
+    data=""
+    try:
+        data = r.recognize_google(audio)
+        print("you said: "+data)
+        password(data) #sending the input data to check for password
+    except sr.UnknownValueError:
+        print("you have entered a wrong password, mini won't work BYE.....")
+    except sr.RequestError as e:
+        print("request result from google speech recognition service error"+e)
+    return data
 
 
 
 # this is the main part 
-allowences=5
+allowences=3 #the user has three chance to enter the correct choice 
+print(" ")
 print(":::::_WELLCOME USER_:::::")
-print()
+print(" ")
 print("please select how you want to run me(mi_ni)")
+print(" ")
 print("Enter: 1: for text command")
 print("  or   2: for voice command")
+print(" ")
 while allowences>0:
     choice = input("enter choice: ")
     if choice=="1":
         print("you have selcted to procced with text commands")
         text = input("enter password: ") # its for entering the password/wake word using keyboard
-        text = text.lower() #converting the input data to lower case 
-        text_password(text)
+        password(text)
         break
     elif choice=="2":
-        voice_password() 
+        recordaudio()
         break
     else:
-        print("invalid input you have more "+p+" out of 5 tries left") 
+        print("invalid input you have more "+ str(allowences) +" out of 5 tries left") 
     
     allowences = allowences-1
